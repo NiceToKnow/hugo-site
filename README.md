0c90746f9cea7fb80ae47c73136c7e02a1e69074
Jannik Arndt <jannik@jannikarndt.de>
2016-11-19 12:05:03 +0100

    Revert

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Musicista Hugo Site Branch
-
-This branch keeps the sources for http://jannikarndt.github.io/Musicista/
-
-Pages are generated into the `public` folder, which is a git submodule to the `gh-pages` branch.
+# NiceToKnow Main Pages
--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-git fetch
-if [[ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]]
-	then echo "\033[31mCurrent branch is not up-to-date, please pull first!\033[0m"
-	exit
-fi
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-echo "\033[2m"
+echo -e "\033[2m"
 # Build the project.
-hugooutput="$(hugo)"
-echo "$hugooutput"
-echo "\033[0m"
+hugo
+echo -e "\033[0m"
 
-if echo "$hugooutput" | grep "ERROR"
-  then echo "\033[31mError during build, cancelling deployment. 🙁\033[0m"
-  exit
-fi
-
+# Go To Public folder
+cd public
 # Add changes to git.
 git add -A
 
 # Commit changes.
+msg="rebuilding site `date`"
 if [ $# -eq 1 ]
     then msg="$1"
 else
-    echo "\033[1mCommit message: "
-    read "newmsg"
-    echo "\033[0m"
-    msg="$newmsg"
+    echo -e "\033[1mCommit message: "
+    read newmsg
+    echo -e "\033[0m"
+    msg=newmsg
 fi
 
 git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:JannikArndt/Musicista.git gh-pages
-echo "New version is deployed 👍"
+
+# Come Back
+cd ..
